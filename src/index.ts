2684584983f8040e9f8dace3bd211cb8bc068413
chick <chickchok06@gmail.com>
2026-06-24 01:38:54 +0900

    포트변경

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Elysia().get(
   () => videos[Math.floor(videos.length * Math.random())],
 );
 
-app.listen(process.env.PORT ?? 3000, () => {
+app.listen(3000, () => {
   console.log(`🦊 Elysia is running at port ${process.env.PORT ?? 3000}`);
 });
 
